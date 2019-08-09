4e23fa396991aefbbb7a05493f87ee2750f6a71e
SongKai <kk99898@163.com>
2019-08-09 22:40:27 +0800

    remove nofile

--- yanpeishi/Node/qwer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var path = require('path');
-
-// 解析当前目录:
-var workDir = path.resolve('.'); // '/Users/michael'
-
-// 组合完整的文件路径:当前目录+'pub'+'index.html':
-var filePath = path.join(workDir, 'pub', 'index.html');
